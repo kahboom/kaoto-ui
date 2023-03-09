adbf70ca1f8ce78ddbbc56abd5a4616a7478cb5d
kahboom <rachelyordan@gmail.com>
2023-03-09 16:14:06 +0000

    chore: add dagre + configuration

--- src/components/VisualizationStep.tsx
+++ src/components/VisualizationStep.tsx
@@ -170,7 +170,8 @@ const VisualizationStep = ({ data }: NodeProps<IVizStepNodeData>) => {
             <Handle
               isConnectable={false}
               type="target"
-              position={visualizationStore.layout === 'RIGHT' ? Position.Left : Position.Top}
+              // position={visualizationStore.layout === 'RIGHT' ? Position.Left : Position.Top}
+              position={visualizationStore.layout === 'LR' ? Position.Left : Position.Top}
               id="a"
               style={{ borderRadius: 0 }}
             />
@@ -200,7 +201,8 @@ const VisualizationStep = ({ data }: NodeProps<IVizStepNodeData>) => {
             <Handle
               isConnectable={false}
               type="source"
-              position={visualizationStore.layout === 'RIGHT' ? Position.Right : Position.Bottom}
+              // position={visualizationStore.layout === 'RIGHT' ? Position.Right : Position.Bottom}
+              position={visualizationStore.layout === 'LR' ? Position.Right : Position.Bottom}
               id="b"
               style={{ borderRadius: 0 }}
             />
@@ -300,7 +302,8 @@ const VisualizationStep = ({ data }: NodeProps<IVizStepNodeData>) => {
               <Handle
                 isConnectable={false}
                 type="target"
-                position={visualizationStore.layout === 'RIGHT' ? Position.Left : Position.Top}
+                // position={visualizationStore.layout === 'RIGHT' ? Position.Left : Position.Top}
+                position={visualizationStore.layout === 'LR' ? Position.Left : Position.Top}
                 id="a"
                 style={{ borderRadius: 0 }}
               />
@@ -314,7 +317,8 @@ const VisualizationStep = ({ data }: NodeProps<IVizStepNodeData>) => {
             {/* RIGHT-SIDE HANDLE FOR EDGE TO CONNECT WITH */}
             <Handle
               type="source"
-              position={visualizationStore.layout === 'RIGHT' ? Position.Right : Position.Bottom}
+              // position={visualizationStore.layout === 'RIGHT' ? Position.Right : Position.Bottom}
+              position={visualizationStore.layout === 'LR' ? Position.Right : Position.Bottom}
               id="b"
               style={{ borderRadius: 0 }}
               isConnectable={false}
